8791264068e587c22912fd42eeeb03f60323d382
Dylan Huang <dylan.p.huang@gmail.com>
2017-03-10 16:13:38 -0600

    Implemented required code for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Not sure why I need to skip the sessions controller but it fixed duplicate routes
+  devise_for :users, skip: [:registrations, :sessions]
+  devise_for :users, controllers: {
+    registrations: 'my_devise/registrations'
+  }
   resources :users
 end
