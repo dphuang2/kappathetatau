3a5434ef6772c21c5f0af4b1f3f2b6890f52a39e
Dylan Huang <dylan.p.huang@gmail.com>
2017-03-10 15:24:39 -0600

    No access alert and Resume link stub

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Redirect to root if user tries to access page they don't have authorization to do so
+  rescue_from Pundit::NotAuthorizedError do
+    redirect_to '/', alert: "You do not have access to this page"
+  end
+
   protected
 
   def configure_permitted_parameters
