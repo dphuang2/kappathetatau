e967d4fa8ea333c2aad4eda25966d6104a78c417
Dylan Huang <dylan.p.huang@gmail.com>
2017-03-10 14:52:10 -0600

    Change :role names and included Pundit for authorization with object passed to it for the index? policy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  enum role: [:user, :vip, :admin]
+  enum role: [:employer, :brother, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= :brother
   end
 
   # Include default devise modules. Others available are:
