a3d505073e09a95109c638bd4e20e96bc1452928
Dylan Huang <dylan.p.huang@gmail.com>
2017-03-17 12:47:04 -0500

    Improved some redirections and home nav link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # The path used after sign in.
+  def after_sign_in_path_for(resource)
+    resumes_path # Redirect to resumes list after sign in
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -54,9 +54,9 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+   def after_sign_up_path_for(resource)
+     edit_path # Redirect Signups to Edit path
+   end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
