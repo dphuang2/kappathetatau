16b741b99abe24c19fa2d310b8f1b00ef008fab0
Dylan Huang <dylan.p.huang@gmail.com>
2017-03-19 11:20:18 -0700

    Finall set up s3 for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,15 +52,16 @@ Rails.application.configure do
     password: Rails.application.secrets.email_provider_password
   }
 
-  ## Amazon S3 Bucket config
+  # Amazon S3 Bucket config
   #config.paperclip_defaults = {
     #storage: :s3,
     #s3_credentials: {
-      #bucket: ENV["S3_BUCKET_NAME"],
+      #bucket: ENV.fetch('S3_BUCKET_NAME'),
       #access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       #secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       #s3_region: ENV.fetch('AWS_REGION'),
-    #}
+    #},
+    #s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   #}
 
   # ActionMailer Config
