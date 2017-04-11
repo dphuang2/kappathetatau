e48c3064cbdb5a2ecd8643aa4df1534a65d94a59
Dylan Huang <dylan.p.huang@gmail.com>
2017-04-10 22:48:59 -0500

    Resume's open up on a new page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   MAJORS = ["Aerospace Engineering (AE)", "Agricultural Engineering (AgE)", "Bioengineering (BioE)", "Chemical and Biomolecular Engineering (ChBE)", "Civil and Environmental Engineering (CEE)", "Computer Engineering (CompE)", "Computer Science (CS)", "Electrical Engineering (EE)", "Engineering Mechanics (EM)", "Engineering Physics (EP)", "General Engineering (GE)", "Industrial Engineering (IE)", "Materials and Science Engineering (MatSE)", "Mechanical Engineering (ME)", "Nuclear, Plasma, and Radiological Engineering (NPRE)"]
 
-  GRADUATION = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+  GRADUATION = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028]
 
   enum role: [:employer, :brother, :admin]
 
